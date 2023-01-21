67c1336d043716e21f60ed47f3ac924a2ece3ff0
Shaun Lecathelinais <shnuq@hotmail.com>
2023-01-21 15:21:48 +0100

    feat: return a user's contracts

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const {sequelize} = require('./model');
 const {getProfile} = require('./middleware/getProfile');
-const {getOneForProfile: getContract} = require('./usecases/contract');
+const {getOneForProfile: getContract, getAllNonTerminatedForProfile: getContracts} = require('./usecases/contract');
 const app = express();
 app.use(bodyParser.json());
 app.set('sequelize', sequelize);
@@ -19,4 +19,16 @@ app.get('/contracts/:id', getProfile, async (req, res) => {
 
     res.json(contract);
 });
+
+/**
+ * Returns a list of non terminated contracts belonging to a user (client or contractor)
+ * @returns contracts
+ */
+app.get('/contracts', getProfile, async (req, res) => {
+    const contract = await getContracts(req.profile.id);
+    if (!contract) return res.status(404).end();
+
+    res.json(contract);
+});
+
 module.exports = app;
--- src/usecases/contract.js
+++ src/usecases/contract.js
@@ -1,3 +1,4 @@
+const {Op} = require('sequelize');
 const model = require('../model');
 
 module.exports.getOneForProfile = async (contractId, profileId) => {
@@ -5,6 +6,19 @@ module.exports.getOneForProfile = async (contractId, profileId) => {
         where: {
             id: contractId,
             ContractorId: profileId,
+        },
+    };
+
+    return model.Contract.findOne(options);
+};
+
+module.exports.getAllNonTerminatedForProfile = async (profileId) => {
+    const options = {
+        where: {
+            status: {
+                [Op.not]: 'terminated',
+            },
+            [Op.or]: [{ContractorId: profileId}, {ClientId: profileId}],
         },
     };
 
