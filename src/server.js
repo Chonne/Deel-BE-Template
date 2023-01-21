e8b3618b5ec95b221a08e2747959eb3c8fe76cd3
Shaun Lecathelinais <shnuq@hotmail.com>
2023-01-21 14:28:58 +0100

    chore: reformat files

--- src/server.js
+++ src/server.js
@@ -3,12 +3,12 @@ const app = require('./app');
 init();
 
 async function init() {
-  try {
-    app.listen(3001, () => {
-      console.log('Express App Listening on Port 3001');
-    });
-  } catch (error) {
-    console.error(`An error occurred: ${JSON.stringify(error)}`);
-    throw error;
-  }
+    try {
+        app.listen(3001, () => {
+            console.log('Express App Listening on Port 3001');
+        });
+    } catch (error) {
+        console.error(`An error occurred: ${JSON.stringify(error)}`);
+        throw error;
+    }
 }
